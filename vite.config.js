37a9c29cd4b014e8053a2bad8f485ba39017d205
Pusarla Vinay <23bis70126@cuchd.in>
2026-02-13 10:20:18 +0530

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest: {
-        name: 'FSD Experiment App',
-        short_name: 'FSD App',
-        description: 'Advanced Frontend Development',
-        theme_color: '#ffffff',
-        background_color: '#ffffff',
-        display: 'standalone',
-        icons: [
-          {
-            src: '/icon-192x192.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: '/icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png'
-          }
-        ]
-      }
-    })
-  ]
+  plugins: [react()],
+  base: "./"
 })
